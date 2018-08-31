ee666929303df9dd928c349bb8255fb8762178cc
icyhearts <icyhearts@163.com>
2018-08-31 16:57:27 +0800

    add ch8/makefile

--- gitadd.sh
+++ gitadd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git add gitadd.sh ch13Daemon/*.c ch13Daemon/makefile mylib/ ch8_process/*.c 
+git add gitadd.sh ch13Daemon/*.c ch13Daemon/makefile mylib/ ch8_process/*.c ch8_process/makefile
