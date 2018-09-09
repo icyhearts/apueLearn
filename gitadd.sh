22b2cbf5789e0d56fffd6bef297775281852fbd7
icyhearts <icyhearts@163.com>
2018-09-09 11:32:09 +0800

    add mand

--- gitadd.sh
+++ gitadd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git add gitadd.sh ch13Daemon/*.c ch13Daemon/makefile mylib/ ch8_process/*.c ch8_process/makefile
+git add gitadd.sh ch13Daemon/*.c ch13Daemon/makefile mylib/ ch8_process/*.c ch8_process/makefile ch14AdvanceIO/c14n*.c
